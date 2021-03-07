4a580d29946d933ac163bdcbe7410d689db6a4c2
Tomás Malvissi <tomasmalvissi@hotmail.com>
2021-03-07 18:42:50 -0300

    add clientes and productos controllers

---
+++ SGV/SGV.WEB/Controllers/ClientesController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGV.DATA;
+using SGV.WEB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGV.WEB.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ClientesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Cliente> listClientes = _context.Clientes;
+            return View(listClientes);
+        }
+
+        //HTTP GET DEL CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Cliente cliente) 
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Add(cliente);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Nuevo cliente creado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET DEL UPDATE
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound(); 
+            }
+
+            var cliente = _context.Clientes.Find(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Update(cliente);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Cliente actualizado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET DEL DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var cliente = _context.Clientes.Find(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCliente(int? id)
+        {
+            var cliente = _context.Clientes.Find(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Remove(cliente);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Cliente eliminado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SGV/SGV.WEB/Controllers/ProductosController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGV.DATA;
+using SGV.WEB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGV.WEB.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Producto> listaProd = _context.Productos;
+            return View(listaProd);
+        }
+
+        //HTTP GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Producto prod)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Productos.Add(prod);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Nuevo producto cargado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET DEL UPDATE PARA OBTENER ID
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var producto = _context.Productos.Find(id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Producto prod)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Productos.Update(prod);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Producto actualizado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET DEL DELETE PARA SABER CUAL ELIMINAR
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var producto = _context.Productos.Find(id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteProducto(int? id)
+        {
+            var prod = _context.Productos.Find(id);
+
+            if (prod == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Productos.Remove(prod);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Producto eliminado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
