4023c50e5d5502d4b33636c5e320a1159395223d
Tomás Malvissi <tomasmalvissi@hotmail.com>
2021-03-08 19:49:11 -0300

    add ventas y compras controllers

---
+++ SGV/SGV.WEB/Controllers/ComprasController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGV.DATA;
+using SGV.WEB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGV.WEB.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ComprasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Compra> listCompras = _context.Compras;
+            return View(listCompras);
+        }
+
+        //HTTP GET DEL CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Compras.Add(compra);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Nueva compra creada con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET DEL EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var compra = _context.Compras.Find(id);
+
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Compras.Update(compra);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Compra actualizada con exito.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //HTTP GET DEL DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var compra = _context.Compras.Find(id);
+
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCompra(int? id)
+        {
+            var compra = _context.Compras.Find(id);
+
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Compras.Remove(compra);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Compra eliminada con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SGV/SGV.WEB/Controllers/VentasController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGV.DATA;
+using SGV.WEB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGV.WEB.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public VentasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Venta> listVentas = _context.Ventas;
+            return View(listVentas);
+        }
+
+        //HTTP GET DEL CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Venta venta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Ventas.Add(venta);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Nueva venta cargada con exito.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //HTTP GET DEL EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            var venta = _context.Ventas.Find(id);
+
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Venta venta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Ventas.Update(venta);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Venta actualizada con exito.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //HTTP GET DEL DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            var venta = _context.Ventas.Find(id);
+
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteVenta(int? id)
+        {
+            var venta = _context.Ventas.Find(id);
+
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Ventas.Remove(venta);
+                _context.SaveChanges();
+
+                TempData["Msj"] = "Venta eliminada con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
